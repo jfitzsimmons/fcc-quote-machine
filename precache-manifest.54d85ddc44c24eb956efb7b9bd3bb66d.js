1e0ba76211b579d8a71991be74d1da64b7792701
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-10-08 14:29:39 -0500

    Updates

---
+++ precache-manifest.54d85ddc44c24eb956efb7b9bd3bb66d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "421eb8fcc56cf1be16e5",
+    "url": "/fcc-quote-machine/static/js/runtime~main.421eb8fc.js"
+  },
+  {
+    "revision": "4e5be7855ee2109ba79e",
+    "url": "/fcc-quote-machine/static/js/main.4e5be785.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/fcc-quote-machine/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "4e5be7855ee2109ba79e",
+    "url": "/fcc-quote-machine/static/css/main.4414e372.chunk.css"
+  },
+  {
+    "revision": "9fe7e6c41dc81fe9a9b931123643ca59",
+    "url": "/fcc-quote-machine/index.html"
+  }
+];
